d49eca24574b76cb78c12791ddc00ae1d7879452
Dmitry <lds84pro@gmail.com>
2021-02-18 21:23:19 +0600

    Updates

--- PetProjects/FSD/ToxinRangeSlider/components/toxin-rangeslider/core/const.js
+++
@@ -1,8 +0,0 @@
-var DOMTREE = {
-    handle: '',
-};
-var ERRORS = {
-    undefinedValue: 'Value is undefined. This is not valid value!',
-};
-export { ERRORS, DOMTREE };
-//# sourceMappingURL=const.js.map
